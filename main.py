9d37804c5a59d0aa1b65021c41c4f159732b4bec
knowncold <llLord43@gmail.com>
2020-09-04 16:46:00 +0800

    :sparkles: upgrade to python3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 cpu = CPU.cpu()
 
-ROMPath = 'invaders.rom'    # it should be smaller than 8192bytes
+ROMPath = 'invaders.rom'  # it should be smaller than 8192bytes
 cpu.loadROM(ROMPath)
 cpu.InitMap()
 cpu.reset()
@@ -13,7 +13,7 @@ cpu.reset()
 io = Input.input()
 
 pygame.init()
-size = width, height = 256, 224
+width, height = 256, 224
 size = height, width
 black = 0, 0, 0
 
@@ -21,6 +21,7 @@ surface = pygame.display.set_mode(size)
 pygame.display.set_caption('Invaders')
 surface.fill(black)
 pxarray = pygame.PixelArray(surface)
+
 
 def refresh(processor, pxArray):
     for i in range(height):
@@ -30,25 +31,26 @@ def refresh(processor, pxArray):
             index += 1
             for k in range(8):
                 if (vRAM & 0x01) == 1:
-                    pxArray[i][255-j*8-k] = (255, 255, 255)
+                    pxArray[i][255 - j * 8 - k] = (255, 255, 255)
                 else:
-                    pxArray[i][255-j*8-k] = (0,0,0)
+                    pxArray[i][255 - j * 8 - k] = (0, 0, 0)
                 vRAM >>= 1
+
 
 pygame.display.update()
 fps = 60
 fps_clock = pygame.time.Clock()
 
-while 1:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_c: # coin
-                cpu.io.IN_PORT1 |= 0x01     # 1 player
+            if event.key == pygame.K_c:  # coin
+                cpu.io.IN_PORT1 |= 0x01  # 1 player
             if event.key == pygame.K_1:
                 cpu.io.IN_PORT1 |= 0x04
-            if event.key == pygame.K_SPACE: # fire
+            if event.key == pygame.K_SPACE:  # fire
                 cpu.io.IN_PORT1 |= 0x10
             if event.key == pygame.K_LEFT:
                 cpu.io.IN_PORT1 |= 0x20
@@ -70,4 +72,3 @@ while 1:
     refresh(cpu, pxarray)
     fps_clock.tick(fps)
     pygame.display.update()
-
